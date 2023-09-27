29226e5b01dd4b61d61e5863ae49a47835815d6e
Scrappycow <scrivon@gmail.com>
2023-09-27 11:41:38 -0700

    corrs

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
-# install puppet-lint using Puppet
+# Create a Puppet manifest to install Flask with pip3
 
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+# Ensure pip3 is installed (you can skip this if pip3 is already installed)
+package { 'python3-pip':
+  ensure => installed,
 }
+
+# Install Flask using pip3 with the specified version
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'], # Make sure pip3 is installed first
+}
+
